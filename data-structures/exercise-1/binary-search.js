95cf5eda5290ae2b513b082f6ccf536b6d6e1cde
saidojp <saidlowxp@gmail.com>
2025-09-16 15:00:03 +0500

    algo/data-ex-1

---
+++ data-structures/exercise-1/binary-search.js
@@ -0,0 +1,19 @@
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid; // нашли элемент
+    }
+    if (arr[mid] < target) {
+      left = mid + 1; // ищем справа
+    } else {
+      right = mid - 1; // ищем слева
+    }
+  }
+
+  return -1; // если не найден
+}
